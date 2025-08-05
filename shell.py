2fa453f11f4fcc744fb28f14ceafbf96a5cfaa7b
jaeyoon <chris0258@naver.com>
2025-08-05 15:41:09 +0900

    write read 구현

--- shell.py
+++ shell.py
@@ -1,13 +1,16 @@
 
-class Shell:
-    ...
+import os
 
-if __name__ == "__main__":
-    ...
 class Shell:
 
     def __init__(self):
         self.command = None
+
+    def read_output(self):
+        with open("ssd_output.txt", "r", encoding="utf-8") as f:
+            content = f.read()
+        return content
+
 
     def run_shell(self):
         while True:
@@ -20,10 +23,16 @@ class Shell:
                 print("Shell Exited Successfully.")
                 break
 
-
-
     def run_command(self, command):
-        if command == "fullwrite 0xAAAABBBB\n":
+        commands = command.strip().split(" ")
+        if self.command.startswith("write"):
+            os.system(f"python ssd.py W, {commands[1]}, {commands[2]}")
+            print("[Write] Done")
+        elif self.command.startswith("R"):
+            os.system(f"python ssd.py R, {commands[1]}")
+            result = self.read_output()
+            print(f"[Read] LBA {commands[1]} : {result}")
+        elif command == "fullwrite 0xAAAABBBB\n":
             for _ in range(100):
                 print("[Write] Done")
         elif command == "fullread\n":
--- test_shell.py
+++ test_shell.py
@@ -1,9 +1,11 @@
+from unittest.mock import patch
+
 from shell import Shell
 import sys
 from io import StringIO
 import re
 
-def test_shell_exit(capsys):
+def test_shell_basic(capsys):
     sys.stdin = StringIO("exit\n")
 
     shell = Shell()
@@ -12,14 +14,57 @@ def test_shell_exit(capsys):
     captured = capsys.readouterr()
 
 
-    # captured = capsys.readouterr()
-
     assert captured.out == "Shell>Shell Exited Successfully.\n"
+
+
+def test_run_command_write():
+    with patch.object(Shell, 'run_command', return_value="[Write] Done") as mock_method:
+        shell = Shell()
+        result = shell.run_command("write 3 0x1289CDEF")
+
+        assert result == "[Write] Done"
+
+        mock_method.assert_called_once_with("write 3 0x1289CDEF")
+
+def test_run_command_read():
+    with patch.object(Shell, 'run_command', return_value="[Read] LBA : 0x00000000") as mock_method:
+        shell = Shell()
+        result = shell.run_command("read 30")
+
+        assert result == "[Read] LBA : 0x00000000"
+
+        mock_method.assert_called_once_with("read 30")
+#
+#
+# def test_shell_write(capsys):
+#     sys.stdin = StringIO("W 20 0x1289CDEF\nexit\n")
+#
+#     shell = Shell()
+#     shell.run_shell()
+#
+#     captured = capsys.readouterr()
+#
+#     # captured = capsys.readouterr()
+#
+#     assert captured.out == "Shell> [Write] Done Shell Exited Successfully.\n"
+#
+#
+# def test_shell_read(capsys):
+#     sys.stdin = StringIO("R 20\nexit\n")
+#
+#     shell = Shell()
+#     shell.run_shell()
+#
+#     captured = capsys.readouterr()
+#
+#     # captured = capsys.readouterr()
+#
+#     assert captured.out == "Shell> [Write] Done Shell Exited Successfully.\n"
 
 
 def test_shell_fullwrite(capsys):
     shell = Shell()
-    shell.run_command("fullwrite 0xAAAABBBB\n")
+    shell.run_command("fullwrite 0xAAAABBBB\n")()
     out, err = capsys.readouterr()
     write_count = out.count("[Write] Done\n")
     assert write_count == 100
