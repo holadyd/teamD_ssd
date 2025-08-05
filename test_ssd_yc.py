6dcadab88dd5412133a376ca7a25900681d2c761
yuchan <mo.yuchan.84@gmail.com>
2025-08-05 14:44:12 +0900

    [feature] ssd write test 항목 선언

---
+++ test_ssd_yc.py
@@ -0,0 +1,22 @@
+import pytest
+from pytest_mock import MockFixture
+
+
+@pytest.mark.skip
+def test_write_basic_flow_with_value(): ...
+
+
+@pytest.mark.skip
+def test_write_3nd_arg_is_valid(): ...
+
+
+@pytest.mark.skip
+def test_write_valid_does_not_append_output(): ...
+
+
+@pytest.mark.skip
+def test_write_read_ssd_nand_file(): ...
+
+
+@pytest.mark.skip
+def test_write_create_ssd_nand_file(): ...
