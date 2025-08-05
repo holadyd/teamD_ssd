6a4d8f9b0dde66878e41a1b79abe25f455666e35
jaewon0.min <jaewon0.min@gmail.com>
2025-08-05 15:30:13 +0900

    [test_shell.py 수정] test_shell_fullread 수정 (red)

--- test_shell.py
+++ test_shell.py
@@ -1,6 +1,7 @@
 from shell import Shell
 import sys
 from io import StringIO
+import re
 
 def test_shell_exit(capsys):
     sys.stdin = StringIO("exit\n")
@@ -22,3 +23,14 @@ def test_shell_fullwrite(capsys):
     out, err = capsys.readouterr()
     write_count = out.count("[Write] Done\n")
     assert write_count == 100
+
+
+def test_shell_fullread(capsys):
+    shell = Shell()
+    shell.run_command("fullread\n")
+    out, err = capsys.readouterr()
+
+    # 정규표현식: [Read] LBA XX : 0xAAAAAAAA
+    pattern = r"\[Read\] LBA \d{1,2} : 0x[0-9A-Fa-f]{8}"
+    matches = re.findall(pattern, out)
+    assert len(matches) == 100
