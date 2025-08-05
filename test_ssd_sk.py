0c4e8743bc5b6ed70f33c236821a3c2bc0a2dce9
sungq-lee <sungq2@gmail.com>
2025-08-05 15:50:33 +0900

    [feature] Red 단계 test_read_when_not_written()

--- test_ssd_sk.py
+++ test_ssd_sk.py
@@ -25,3 +25,24 @@ def test_read_2nd_invalid_args():
     assert ret1 == "ERROR"
     assert ret2 == "ERROR"
 
+
+def test_read_when_not_written():
+    ## Arrange
+    command_line1 = "R 99"
+    command_line2 = "R 33"
+
+    command_split1 = command_line1.split()
+    command_split2 = command_line2.split()
+
+    lba1 = command_split1[1]
+    lba2 = command_split2[1]
+
+    ssd = SSD()
+
+    ## act
+    ret1 = ssd.read(lba1)
+    ret2 = ssd.read(lba2)
+
+    # assert
+    assert ret1 == "0x00000000"
+    assert ret2 == "0x00000000"
