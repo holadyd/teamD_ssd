6dc2fca90c0a04ebd36039a6d54c6dd43a70582e
jaewon0.min <jaewon0.min@gmail.com>
2025-08-05 15:21:25 +0900

    test_shell unittest 작성 : test_shell_full_write (red상태)

--- test_shell.py
+++ test_shell.py
@@ -14,3 +14,11 @@ def test_shell_exit(capsys):
     # captured = capsys.readouterr()
 
     assert captured.out == "Shell>Shell Exited Successfully.\n"
+
+
+def test_shell_fullwrite(capsys):
+    shell = Shell()
+    shell.run_command("fullwrite 0xAAAABBBB\n")
+    out, err = capsys.readouterr()
+    write_count = out.count("[Write] Done\n")
+    assert write_count == 100
