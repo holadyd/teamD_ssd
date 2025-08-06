e9baf11e7460d8a69edba7a4e71b75d018603046
jaeyoon <chris0258@naver.com>
2025-08-06 16:05:32 +0900

    shell 코드에서 ssd.py 사용하는 테스크 코드 추가

--- shell.py
+++ shell.py
@@ -70,7 +70,7 @@ class Shell:
             print(f"[Read] LBA {address} : {result}")
 
     def ssd_write(self, address, content, for_script=False):
-        os.system(f"python ssd.py W {address} {content}")
+        os.system(f"python ssd.py W {address} {str(hex(int(content)))}")
         if for_script:
             return
 
