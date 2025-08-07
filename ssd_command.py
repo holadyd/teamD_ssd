3fa18ba731d49fcd8a336397ec56ad1a614e514f
yuchan <mo.yuchan.84@gmail.com>
2025-08-07 13:13:04 +0900

    [feature] erase command validate 기능 추가

--- ssd_command.py
+++ ssd_command.py
@@ -36,12 +36,28 @@ class ReadCommand(SSDCommand):
 
 
 class EraseCommand(SSDCommand):
+    lba_upper_limit = 99
+    lba_lower_limit = 0
+    erase_size_range = 10
+
     def __init__(self, lba, data_size):
         self.lba = lba
         self.data_size = data_size
 
     def validate(self):
-        pass
+        try:
+            lba: int = int(self.lba)  # lba 검증(숫자 여부)
+            if 0 > lba or lba > 99:
+                raise ValueError
+
+            value: int = int(self.data_size)  # size value 검증(숫자 여부)
+            if value < 0 or value > self.erase_size_range:
+                raise ValueError
+            if lba + value > self.lba_upper_limit:
+                raise ValueError
+            return True
+        except (ValueError, TypeError):
+            return False
 
     def make_string(self):
         pass
