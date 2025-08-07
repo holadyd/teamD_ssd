9fa6d3e126a3380abb8c1a3b0615fc34b215c12b
hyunyong <hyunyong.kim@daum.net>
2025-08-07 15:45:29 +0900

    [bugfix] Modified testcases.

--- test_buffer.py
+++ test_buffer.py
@@ -10,7 +10,8 @@ def test_update_buffer_1():
     buf.write_buffer("E 12 3")  # 위의 3개를 합치면 W 20 ~ + E 10 5가 되어야 함
 
     buf.update_buffer()
-    assert buf._buffer == ['1_W_20_0xABCDEFFF', '2_E_10_5', '3_empty', '4_empty', '5_empty']
+    buf.read_buffer()
+    assert buf._buffer == ['1_E_10_5', '2_W_20_0xABCDEFFF', '3_empty', '4_empty', '5_empty']
 
 def test_update_buffer_2():
     buf = Buffer()
@@ -23,6 +24,7 @@ def test_update_buffer_2():
     buf.write_buffer("E 46 5")
 
     buf.update_buffer()
+    buf.read_buffer()
 
     assert buf._buffer == ['1_E_30_10', '2_E_41_10', '3_W_40_0xAAAAAAAA', '4_W_45_0xBBBBBBBB', '5_empty']
 
@@ -38,6 +40,7 @@ def test_update_buffer_3():
     buf.write_buffer("W 51 0xBBBBBBBB")
 
     buf.update_buffer()
+    buf.read_buffer()
 
     assert buf._buffer == ['1_E_31_10', '2_E_41_10', '3_W_37_0xAAAAAAAA', '4_W_51_0xBBBBBBBB', '5_empty']
 
@@ -53,6 +56,7 @@ def test_update_buffer_4():
     buf.write_buffer("E 42 10")
 
     buf.update_buffer()
+    buf.read_buffer()
 
     assert buf._buffer == ['1_E_31_10', '2_E_42_10', '3_W_37_0xAAAAAAAA', '4_W_41_0xBBBBBBBB', '5_empty']
 
@@ -69,6 +73,7 @@ def test_update_buffer_5():
     buf.write_buffer("E 34 2")
 
     buf.update_buffer()
+    buf.read_buffer()
 
     assert buf._buffer == ['1_E_34_2', '2_empty', '3_empty', '4_empty', '5_empty']
 
@@ -82,6 +87,7 @@ def test_update_buffer_6():
     buf.write_buffer("E 12 3")  # 위의 3개를 합치면 W 20 ~ + E 10 5가 되어야 함
 
     buf.update_buffer()
+    buf.read_buffer()
     assert buf._buffer == ['1_E_10_5', '2_empty', '3_empty', '4_empty', '5_empty']
 
 # buffer 생성시  buffer 폴더가 없다면 buffer 폴더 생성 + 파일 초기화 ( {index}_empty )
@@ -114,6 +120,7 @@ def test_buffer_file_should_be_empty_after_buffer_write():
     buf.write_buffer("W 31 0xAAAAAAAA")
     buf.write_buffer("W 32 0xBBBBBBBB")
     buf.write_buffer("W 33 0xCCCCCCCC")
+    buf.read_buffer()
 
     # 모든 파일에 대해 내용이 비어 있는지 확인
     for fname in os.listdir(buf_dir):
@@ -133,6 +140,7 @@ def test_buffer_file_should_be_empty_after_buffer_write():
     buf.write_buffer("W 33 0xCCCCCCCC")
     buf.write_buffer("E 33 2")
     buf.update_buffer()
+    buf.read_buffer()
 
     assert buf._buffer == ['1_W_31_0xAAAAAAAA', '2_W_32_0xBBBBBBBB', '3_E_33_2', '4_empty', '5_empty']
 
